77d527bc06d7d6c971a3e116d0fbdf2421366ce6
DaVinCi <52623427+Blue-Davinci@users.noreply.github.com>
2024-05-22 15:17:47 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,25 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+''' script that returns information about a TODO list progress
+    for a given employee ID'''
+
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{url}/users/{sys.argv[1]}'
+    userId_url = f'{url}/todos?userId={sys.argv[1]}'
+    response = requests.get(user_url)
+    response2 = requests.get(userId_url)
+    EMPLOYEE_NAME = response.json().get('name')
+    NUMBER_OF_DONE_TASKS = len([task for task in response2.json()
+                                if task.get('completed')])
+
+    TOTAL_NUMBER_OF_TASKS = len(response2.json())
+    TASK_TITLE = [task for task in response2.json() if task.get('completed')]
+    print('Employee {} is done with tasks({}/{}):'
+          .format(EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
+
+    for task in TASK_TITLE:
+        print(f'\t {task.get("title")}')
