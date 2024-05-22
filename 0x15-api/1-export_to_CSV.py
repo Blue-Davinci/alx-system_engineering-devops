ed1ac3eb56ff005ef7f548e7c8e7f62db309e4bd
DaVinCi <52623427+Blue-Davinci@users.noreply.github.com>
2024-05-22 15:20:23 +0300

    Update 1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to CSV format."""
+''' export data in the CSV format.'''
+
 import csv
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow(
-            [user_id, username, t.get("completed"), t.get("title")]
-         ) for t in todos]
+    url = 'https://jsonplaceholder.typicode.com'
+    user_url = f'{url}/users/{sys.argv[1]}'
+    userId_url = f'{url}/todos?userId={sys.argv[1]}'
+    response = requests.get(user_url)
+    response2 = requests.get(userId_url)
+    user_id = response.json().get('id')
+    username = response.json().get('username')
+    list_userId = [task for task in response2.json()]
+
+    with open(f'{sys.argv[1]}.csv', mode='w', newline='') as file:
+        csv_writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for task in list_userId:
+            csv_writer.writerow([f'{user_id}', f'{username}',
+                                 f'{task.get("completed")}',
+                                 f'{task.get("title")}'])
